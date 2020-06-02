17eb0da0bffbfe37f8079c047295707347f3f0b1
Prashanti Anderson <psunander@gmail.com>
2020-06-02 19:36:28 +0000

    Refactored comments feature to include comments class and seperating servlets which do different tasks for easier readability and ease of adding new features

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,15 @@
+package com.google.sps.data;
+
+/** An comment on a webpage. */
+public final class Comment {
+	
+  private final long id;
+  private final String content;
+  private final long timestamp;
+	      
+  public Comment(long id, String content, long timestamp) {
+    this.id = id;
+    this.content = content;
+    this.timestamp = timestamp;
+  }
+}
